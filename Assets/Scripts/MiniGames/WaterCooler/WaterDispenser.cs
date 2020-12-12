3be50040727eeee58408e8e36b8118d13c570835
Jason <jak4694@rit.edu>
2020-12-12 18:29:20 -0500

    Base setup of water cooler

---
+++ Assets/Scripts/MiniGames/WaterCooler/WaterDispenser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Class to handle dispensing water
+/// </summary>
+public class WaterDispenser : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject water = null;
+    private bool activated = false;
+
+    public void ToggleDispenser()
+    {
+        activated = !activated;
+        if(water)
+        {
+            water.SetActive(activated);
+        }
+    }
+}
