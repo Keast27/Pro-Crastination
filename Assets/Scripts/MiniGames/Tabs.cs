08b3346fbfcab24ca64dda0a3a0ec6aad654951f
Kayleigh <keast1027@gmail.com>
2020-12-12 03:13:02 -0500

    Floor and walls are tile palettes and mini games are barely started

---
+++ Assets/Scripts/MiniGames/Tabs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tabs : MonoBehaviour
+{
+    public GameObject workTab;
+    public GameObject socialTab;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
