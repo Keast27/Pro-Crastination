a6b436bd36992b1531f5a9e2d4ef132d85f07c9e
Cristian <cristiandc10@live.com>
2020-12-12 22:08:12 -0500

    Added Dialogue to objects, doesn't fully work yet

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -7,24 +7,4 @@ public class Dialogue : ScriptableObject
 {
     [TextArea(3, 10)]
     public string[] sentences; // Dialogue
-    //private Queue<string> sentenceQueue;
-
-    /*
-    public Queue<string> Sentences
-    {
-        get
-        {
-            if (sentenceQueue == null)
-            {
-                sentenceQueue = new Queue<string>();
-                foreach (string sentence in sentences)
-                {
-                    sentenceQueue.Enqueue(sentence);
-                }
-            }
-
-            return sentenceQueue;
-        }
-    } // Dialogue
-    */
 }
