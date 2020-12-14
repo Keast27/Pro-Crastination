d250999bacb9733b02d85d7ec0fbcedbec6ee254
Kayleigh <keast1027@gmail.com>
2020-12-13 23:45:23 -0500

    Text added

--- Assets/Scripts/MiniGames/MemeFeed.cs
+++ Assets/Scripts/MiniGames/MemeFeed.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class MemeFeed : MonoBehaviour, IPointerDownHandler
 {
+    public TextMeshPro captioner;
     private SpriteRenderer spriteRenderer;
     public Sprite[] spriteArray;
     public int currentSprite;
 
+    public string[] captions;
     public void OnPointerDown(PointerEventData eventData)
     {
         ChangeSprite();
@@ -18,6 +21,7 @@ public class MemeFeed : MonoBehaviour, IPointerDownHandler
 
     void Start()
     {
+        captioner.text = captions[currentSprite];
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
@@ -29,6 +33,7 @@ public class MemeFeed : MonoBehaviour, IPointerDownHandler
     void ChangeSprite()
     {
         spriteRenderer.sprite = spriteArray[currentSprite];
+        captioner.text = captions[currentSprite];
         currentSprite++;
 
         if (currentSprite >= spriteArray.Length)
