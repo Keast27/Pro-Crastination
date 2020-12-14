c8a621f35e4d3ef184495670b0993cd5085a8cd4
Kayleigh <keast1027@gmail.com>
2020-12-13 19:17:18 -0500

    Meme cycle works

---
+++ Assets/Scripts/MiniGames/MemeFeed.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+public class MemeFeed : MonoBehaviour, IPointerDownHandler
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
+    public int currentSprite;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ChangeSprite();
+        Debug.Log("Click");
+    }
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+
+    }
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = spriteArray[currentSprite];
+        currentSprite++;
+
+        if (currentSprite >= spriteArray.Length)
+        {
+            currentSprite = 0;
+        }
+    }
+}
